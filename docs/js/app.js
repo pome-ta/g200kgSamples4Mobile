5531016f4ec3f024a3dcd2edf163980f50ad9457
pome-ta <53405097+pome-ta@users.noreply.github.com>
2022-04-04 00:17:51 +0900

    up

--- docs/js/app.js
+++ docs/js/app.js
@@ -1,3 +1,40 @@
 'use strict';
 
+const tapStart = typeof document.ontouchstart !== 'undefined' ? 'touchstart' : 'mousedown';
+
+/*
+document.addEventListener('DOMContentLoaded', () => {
+  console.log('DOMContentLoaded');
+});
+*/
+
+/*
+const playButton = document.createElement('button');
+playButton.textContent = 'play';
+document.body.appendChild(playButton);
+*/
+/*
+const playButton = document.createElement('input');
+  playButton.type = 'button';
+  playButton.value = 'play';
+*/
+
+function Play() {
+  console.log('call');
+  const AudioContext = window.AudioContext || window.webkitAudioContext;
+  const audioctx = new AudioContext();
+  const osc = new OscillatorNode(audioctx);
+  osc.connect(audioctx.destination);
+  osc.start();
+}
+
+
+let playButton;
+document.addEventListener('DOMContentLoaded', () => {
+  playButton = document.createElement('button');
+  playButton.textContent = 'play';
+  playButton.addEventListener(tapStart, () => Play());
+  document.body.appendChild(playButton);
+});
+
 
