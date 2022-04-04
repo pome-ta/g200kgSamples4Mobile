e86eb9adcb75e8a088f1920509466414bbd96f3a
pome-ta <53405097+pome-ta@users.noreply.github.com>
2022-04-04 10:36:49 +0900

    call sound

--- docs/js/app.js
+++ docs/js/app.js
@@ -2,39 +2,22 @@
 
 const tapStart = typeof document.ontouchstart !== 'undefined' ? 'touchstart' : 'mousedown';
 
-/*
-document.addEventListener('DOMContentLoaded', () => {
-  console.log('DOMContentLoaded');
-});
-*/
 
-/*
 const playButton = document.createElement('button');
+playButton.type = 'button';
 playButton.textContent = 'play';
+playButton.addEventListener(tapStart, Play);
 document.body.appendChild(playButton);
-*/
-/*
-const playButton = document.createElement('input');
-  playButton.type = 'button';
-  playButton.value = 'play';
-*/
+
 
 function Play() {
-  console.log('call');
-  const AudioContext = window.AudioContext || window.webkitAudioContext;
+  //console.log('call');
+  //const AudioContext = window.AudioContext || window.webkitAudioContext;
   const audioctx = new AudioContext();
   const osc = new OscillatorNode(audioctx);
   osc.connect(audioctx.destination);
   osc.start();
 }
 
-
-let playButton;
-document.addEventListener('DOMContentLoaded', () => {
-  playButton = document.createElement('button');
-  playButton.textContent = 'play';
-  playButton.addEventListener(tapStart, () => Play());
-  document.body.appendChild(playButton);
-});
 
 
