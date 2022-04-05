4743b6c7ec2fa75bcba84456b349cea9f93b6647
pome-ta <53405097+pome-ta@users.noreply.github.com>
2022-04-05 15:19:21 +0900

    03-2

--- docs/js/app.js
+++ docs/js/app.js
@@ -2,39 +2,17 @@
 
 const tapStart = typeof document.ontouchstart !== 'undefined' ? 'touchstart' : 'mousedown';
 
-const capitalize = str => {
-  if (typeof str !== 'string' || !str) return `${str}`;
-  return str.charAt(0).toUpperCase() + str.slice(1).toLowerCase();
-};
 
-
-let playing = false;
-let osc, gain;
 function Play() {
-  if (playing) return;
   const audioctx = new AudioContext();
-  osc = new OscillatorNode(audioctx);
-  gain = new GainNode(audioctx);
-  osc.connect(gain).connect(audioctx.destination);
-  osc.start();
-  playing = true;
+  const t0 = audioctx.currentTime;
+  for (let i = 0; i < 1; i++) {
+    const osc = new OscillatorNode(audioctx);
+    osc.connect(audioctx.destination);
+    osc.start(t0 + i * 0.5 + 0.5);
+    osc.stop(t0 + i * 0.5 + 0.6);
+  }
 }
-
-
-function Setup() {
-  const typeValue = waveSelect.value;
-  const freqValue = freq.value;
-  const levelValue = level.value;
-  
-  freqdisp.textContent = freqValue;
-  leveldisp.textContent = levelValue;
-  
-  osc.type = typeValue;
-  osc.frequency.value = freqValue;
-  gain.gain.value = levelValue;
-  
-}
-
 
 
 const playButton = document.createElement('button');
@@ -42,78 +20,6 @@ playButton.type = 'button';
 playButton.textContent = 'play';
 playButton.addEventListener(tapStart, Play);
 
-
-/* type */
-const waveSelect = document.createElement('select');
-waveSelect.id = 'type';
-const waveTypes = ['sine', 'square', 'sawtooth', 'triangle'];
-for (const wType of waveTypes) {
-  const option = document.createElement('option');
-  option.value = wType.toLowerCase();
-  option.text = capitalize(wType);
-  waveSelect.appendChild(option);
-}
-waveSelect.addEventListener('change', Setup);
-
-/* Freq(Hz) */
-const freq = document.createElement('input');
-freq.type = 'range';
-freq.id = 'freq';
-freq.min = 50;
-freq.max = 3000;
-freq.value = 440;
-freq.addEventListener('input', Setup);
-
-//const freqdisp = document.createElement('div');
-const freqdisp = document.createElement('td');
-freqdisp.id = 'freqdisp';
-freqdisp.textContent = freq.value;
-
-
-/* Level */
-const level = document.createElement('input');
-level.type = 'range';
-level.id = 'level';
-level.min = 0.0;
-level.max = 1.0;
-level.step = 0.01;
-level.value = 0.5;
-level.addEventListener('input', Setup);
-
-//const leveldisp = document.createElement('div');
-const leveldisp = document.createElement('td');
-leveldisp.id = 'freqdisp';
-leveldisp.textContent = level.value;
-
-
-const controllerObj = {
-  'Type': [waveSelect],
-  'Freq(Hz)': [freq, freqdisp],
-  'Level': [level, leveldisp]
-};
-
-const tbl = document.createElement('table');
-const tblBody = document.createElement('tbody');
-for (const key of Object.keys(controllerObj)) {
-  const row = document.createElement('tr');
-  const th = document.createElement('th');
-  th.textContent = key;
-  row.appendChild(th);
-  for (const value of controllerObj[key]) {
-    if (value === 'TD') {
-      row.appendChild(value);
-    } else {
-      const td = document.createElement('td');
-      td.appendChild(value);
-      row.appendChild(td);
-    }
-  }
-  tblBody.appendChild(row);
-}
-
-// xxx: 関数つくろうかな
 const body = document.body;
 body.appendChild(playButton);
-tbl.appendChild(tblBody);
-body.appendChild(tbl);
 
