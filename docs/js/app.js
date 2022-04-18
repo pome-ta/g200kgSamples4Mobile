710afc716e31becc3e9cd4c706c3ecf7e9baefc1
pome-ta <53405097+pome-ta@users.noreply.github.com>
2022-04-18 22:51:43 +0900

    add wav

--- docs/js/app.js
+++ docs/js/app.js
@@ -1,26 +1,41 @@
 'use strict';
 
+// xxx: click とかtouch とか
 const { tapDown, tapMove, tapUp } = {
   tapDown: typeof document.ontouchstart !== 'undefined' ? 'touchstart' : 'mousedown',
   tapMove: typeof document.ontouchmove !== 'undefined' ? 'touchmove' : 'mousemove',
   tapUp: typeof document.ontouchend !== 'undefined' ? 'touchend' : 'mouseup',
 };
 
+function createPlayButton() {
+  const bttn = document.createElement('button');
+  bttn.id = 'play';
+  bttn.type = 'button';
+  bttn.textContent = 'Play';
+  return bttn;
+}
 
+async function loadSample(actx, uri) {
+  const res = await fetch(uri);
+  const arraybuf = await res.arrayBuffer();
+  await sleep(1000);
+  console.log('hoge');
+  return actx.decodeAudioData(arraybuf);
+}
 
-const playButton = document.createElement('button');
-playButton.id = 'play';
-playButton.type = 'button';
-playButton.textContent = 'Play';
+const playButton = createPlayButton();
+
 //playButton.addEventListener(tapDown, Start);
 
 const body = document.body;
 body.appendChild(playButton);
 
-const soundPath = '/System/Library/Audio/UISounds/SIMToolkitPositiveACK.caf';
+//const soundPath = '/System/Library/Audio/UISounds/SIMToolkitPositiveACK.caf';
+const soundPath = './sounds/440_out.wav';
 
 window.addEventListener('load', async() => {
   const audioctx = new AudioContext();
+  //const sound = await loadSample(audioctx, soundPath);
   const sound = await LoadSample(audioctx, soundPath);
   
   playButton.addEventListener(tapDown, () => {
@@ -40,5 +55,6 @@ window.addEventListener('load', async() => {
       })
     });
   }
+  
   
 });
