5c9279fb906da89ae8bbb6c729b734dddeb4e123
pome-ta <53405097+pome-ta@users.noreply.github.com>
2022-04-19 13:05:25 +0900

    06-2

--- docs/js/app.js
+++ docs/js/app.js
@@ -2,17 +2,19 @@
 
 // xxx: click とかtouch とか
 const { tapDown, tapMove, tapUp } = {
-  tapDown: typeof document.ontouchstart !== 'undefined' ? 'touchstart' : 'mousedown',
-  tapMove: typeof document.ontouchmove !== 'undefined' ? 'touchmove' : 'mousemove',
+  tapDown:
+    typeof document.ontouchstart !== 'undefined' ? 'touchstart' : 'mousedown',
+  tapMove:
+    typeof document.ontouchmove !== 'undefined' ? 'touchmove' : 'mousemove',
   tapUp: typeof document.ontouchend !== 'undefined' ? 'touchend' : 'mouseup',
 };
 
-window.addEventListener('load', async() => {
+window.addEventListener('load', async () => {
   sound = await loadSample(audioctx, soundPath);
 });
 
-
-const sleep = waitTime => new Promise(resolve => setTimeout(resolve, waitTime));
+const sleep = (waitTime) =>
+  new Promise((resolve) => setTimeout(resolve, waitTime));
 async function loadSample(actx, uri) {
   const res = await fetch(uri);
   const arraybuf = await res.arrayBuffer();
@@ -24,10 +26,6 @@ const soundPath = './sounds/440_out.wav';
 const audioctx = new AudioContext();
 let sound = null;
 
-
-
-
-
 function createPlayButton() {
   const bttn = document.createElement('button');
   bttn.id = 'play';
@@ -38,11 +36,10 @@ function createPlayButton() {
 
 const playButton = createPlayButton();
 playButton.addEventListener(tapDown, () => {
-  const src = new AudioBufferSourceNode(audioctx, {buffer:sound});
+  const src = new AudioBufferSourceNode(audioctx, { buffer: sound });
   src.connect(audioctx.destination);
   src.start();
 });
 
 const body = document.body;
 body.appendChild(playButton);
-
