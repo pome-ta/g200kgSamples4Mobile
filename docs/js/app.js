6cace20cfbf763f3616cc3bc552e4adbaa520db7
pome-ta <53405097+pome-ta@users.noreply.github.com>
2022-05-25 15:07:49 +0900

    07 setup

--- docs/js/app.js
+++ docs/js/app.js
@@ -41,5 +41,102 @@ playButton.addEventListener(tapDown, () => {
   src.start();
 });
 
+
+
+/* setup document element */
+
+const FREQUENCY_VALUE = 5.0;
+const GAIN_VALUE = 10.0;
+
 const body = document.body;
 body.appendChild(playButton);
+
+
+function createInputRange(rangeObj) {
+  const {id, min, max, value, step=''} = rangeObj;
+  const element = document.createElement('input');
+  element.type = 'range';
+  element.id = id;
+  element.min = min;
+  element.max = max;
+  element.step = step;
+  element.value = value;
+  return element;
+}
+
+const lfofreqObj = {
+  id: 'lfofreq',
+  min: 0.1,
+  max: 20.0,
+  step: 0.1,
+  value: FREQUENCY_VALUE
+};
+
+const depthObj = {
+  id: 'depth',
+  min: 0.0,
+  max: 100.0,
+  value: GAIN_VALUE
+};
+
+const oscfreqObj = {
+  id: 'oscfreq',
+  min: 50.0,
+  max: 3000.0,
+  value: 440.0
+};
+
+const lfofreq = createInputRange(lfofreqObj);
+const depthfreq = createInputRange(depthObj);
+const oscfreq = createInputRange(oscfreqObj);
+
+/*
+lfofreq.addEventListener('input', Setup);
+depthfreq.addEventListener('input', Setup);
+oscfreq.addEventListener('input', Setup);
+*/
+//document.addEventListener('DOMContentLoaded', Setup);
+
+const lfofreqval = document.createElement('td');
+lfofreqval.id = 'lfofreqval';
+
+const depthval = document.createElement('td');
+depthval.id = 'depthval';
+
+const oscfreqval = document.createElement('td');
+oscfreqval.id = 'oscfreqval';
+
+
+const controllerObjs = {
+  'LFO Freq': [lfofreq, lfofreqval],
+  'Depth': [depthfreq, depthval],
+  'OSC Freq': [oscfreq, oscfreqval]
+};
+
+const tbl = document.createElement('table');
+const tblBody = document.createElement('tbody');
+for (const key of Object.keys(controllerObjs)) {
+  const row = document.createElement('tr');
+  const th = document.createElement('th');
+  th.textContent = key;
+  row.appendChild(th);
+  for (const value of controllerObjs[key]) {
+    if (value === 'TD') {
+      row.appendChild(value);
+    } else {
+      const td = document.createElement('td');
+      td.appendChild(value);
+      row.appendChild(td);
+    }
+  }
+  tblBody.appendChild(row);
+}
+
+
+
+const mainTitleHeader = document.createElement('h1');
+mainTitleHeader.textContent = 'AudioParam Automation';
+
+body.appendChild(mainTitleHeader);
+tbl.appendChild(tblBody);
+body.appendChild(tbl);
