c34cbaa1b111dc6be96f2b82a4e64400beb78832
pome-ta <53405097+pome-ta@users.noreply.github.com>
2022-05-31 20:51:57 +0900

    formatting

--- docs/js/app.js
+++ docs/js/app.js
@@ -4,50 +4,49 @@
 function capitalize(str) {
   if (typeof str !== 'string' || !str) return str;
   return str.charAt(0).toUpperCase() + str.slice(1).toLowerCase();
-};
-
+}
 
 /* create document node element funcs */
-function createButton(idName, textContent=null) {
+function createButton(idName, textContent = null) {
   const element = document.createElement('button');
-        element.style.width = '100%';
-        element.style.height = '4rem';
-        element.type = 'button';
-        element.id = idName;
-        element.textContent = (textContent) ? textContent :  capitalize(idName);
+  element.style.width = '100%';
+  element.style.height = '4rem';
+  element.type = 'button';
+  element.id = idName;
+  element.textContent = textContent ? textContent : capitalize(idName);
   return element;
 }
 
-
-function createLabel(idName, textContent=null) {
+function createLabel(idName, textContent = null) {
   const element = document.createElement('p');
-        element.id = idName;
-        element.textContent = (textContent) ? textContent :  capitalize(idName);
+  element.id = idName;
+  element.style.margin = '0';
+  element.textContent = textContent ? textContent : capitalize(idName);
   return element;
 }
 
 function createInputRange(rangeObj) {
   const { id, min, max, value, step = '' } = rangeObj;
   const element = document.createElement('input');
-        element.type = 'range';
-        element.id = id;
-        element.min = min;
-        element.max = max;
-        element.step = step;
-        element.value = value;
-        element.style.width = '100%';
+  element.type = 'range';
+  element.id = id;
+  element.min = min;
+  element.max = max;
+  element.step = step;
+  element.value = value;
+  element.style.width = '100%';
   return element;
 }
 
 function createSelectOpiton(selectObj, typestr) {
-  const {id} = selectObj;
+  const { id } = selectObj;
   const element = document.createElement('select');
-        element.id = id;
-        //element.addEventListener('change', Setup);
+  element.id = id;
+  //element.addEventListener('change', Setup);
   for (const type of typestr) {
     const option = document.createElement('option');
-          option.value = type.toLowerCase();
-          option.text = capitalize(type);
+    option.value = type.toLowerCase();
+    option.text = capitalize(type);
     element.appendChild(option);
   }
   return element;
@@ -57,79 +56,90 @@ function createControllerObjs(objArray) {
   const selectObj = 'selectObj';
   const inputObj = 'inputObj';
   const pObj = 'pObj';
-  
+
   const controllerObjs = {};
   for (const obj of objArray) {
-    const selectElement = (Object.keys(obj).some(key => key === selectObj)) ? createSelectOpiton(obj[selectObj], typeStr) : null;
-    
-    const inputElement = (Object.keys(obj).some(key => key === inputObj)) ? createInputRange(obj[inputObj]) : null;
-    
-    const _label = (inputElement) ? inputElement.value : null;
-    const pElement = (Object.keys(obj).some(key => key === pObj)) ? createLabel(obj[pObj], _label) : null;
-    
-    
-    controllerObjs[obj['objName']] = [selectElement,inputElement, pElement].filter(ele => ele);
+    const selectElement = Object.keys(obj).some((key) => key === selectObj)
+      ? createSelectOpiton(obj[selectObj], typeStr)
+      : null;
+
+    const inputElement = Object.keys(obj).some((key) => key === inputObj)
+      ? createInputRange(obj[inputObj])
+      : null;
+
+    const _label = inputElement ? inputElement.value : null;
+    const pElement = Object.keys(obj).some((key) => key === pObj)
+      ? createLabel(obj[pObj], _label)
+      : null;
+
+    controllerObjs[obj['objName']] = [
+      selectElement,
+      inputElement,
+      pElement,
+    ].filter((ele) => ele);
   }
   return controllerObjs;
 }
 
-function createTableHeader(values) {
-  const tr = document.createElement('tr');
-  for (const value of values) {
-    const th = document.createElement('th');
-          th.textContent = value
-          th.style.whiteSpace = 'nowrap';
-    tr.appendChild(th);
-  }
-  return tr;
-}
-
-function createControllerTable(controllers, customHeader=null) {
+function createControllerTable(controllers) {
   const tblBody = document.createElement('tbody');
   for (const key of Object.keys(controllers)) {
     const th = document.createElement('th');
-          th.textContent = key;
-          th.style.whiteSpace = 'nowrap';
-          th.style.width = '0%';
+    th.textContent = key;
+    th.style.whiteSpace = 'nowrap';
+    th.style.width = '0%';
     const tr = document.createElement('tr');
-          tr.appendChild(th);
+    tr.appendChild(th);
     for (const value of controllers[key]) {
       const td = document.createElement('td');
-            td.style.width = (value.nodeName === 'SELECT') ? '0%' : '100%';
-            td.appendChild(value);
+      td.style.width = value.nodeName === 'SELECT' ? '0%' : '100%';
+      td.appendChild(value);
       tr.appendChild(td);
     }
     tblBody.appendChild(tr);
   }
-  (customHeader) ? tblBody.prepend(createTableHeader(customHeader)) : null;
   const tbl = document.createElement('table');
-        tbl.style.width = '100%';
-        tbl.appendChild(tblBody);
+  tbl.style.width = '100%';
+  tbl.appendChild(tblBody);
   return tbl;
 }
 
-
-function setAppendChild(nodes, parentNode=document.body) {
+function setAppendChild(nodes, parentNode = document.body) {
   let preNode = parentNode;
-  nodes.forEach(node => {
-    (Array.isArray(node)) ? setAppendChild(node, preNode) : parentNode.appendChild(node);
+  nodes.forEach((node) => {
+    Array.isArray(node)
+      ? setAppendChild(node, preNode)
+      : parentNode.appendChild(node);
     preNode = node;
   });
 }
 
-
 /* setup document node element */
 const mainTitleHeader = document.createElement('h2');
-      mainTitleHeader.textContent = 'BiquadFilter Test';
+mainTitleHeader.textContent = 'BiquadFilter Test';
+
+const buttonDiv = document.createElement('div');
+buttonDiv.style.width = '100%';
+const playnoiseButton = createButton('playnoise', 'Play Noise');
+const stopButton = createButton('stop');
 
 /* create controller objs */
-const typeStr = ['LPF', 'HPF', 'BPF', 'LowShelf', 'HighShelf', 'Peaking', 'Notch', 'AllPass'];
+const typeStr = [
+  'LPF',
+  'HPF',
+  'BPF',
+  'LowShelf',
+  'HighShelf',
+  'Peaking',
+  'Notch',
+  'AllPass',
+];
 
 const selectTypeObj = {
   selectObj: {
-    id: 'type'
+    id: 'type',
   },
-  objName: 'Type'
+  objName: 'Type',
 };
 
 const freqvalObj = {
@@ -137,13 +147,13 @@ const freqvalObj = {
     id: 'freq',
     min: 100,
     max: 20000,
-    value: 5000
+    value: 5000,
   },
   pObj: {
     id: 'freqval',
-    label: ''
+    label: '',
   },
-  objName: 'Freq'
+  objName: 'Freq',
 };
 
 const qvalObj = {
@@ -152,13 +162,13 @@ const qvalObj = {
     min: 0,
     max: 50,
     step: 0.5,
-    value: 5
+    value: 5,
   },
   pObj: {
     id: 'qval',
-    label: ''
+    label: '',
   },
-  objName: 'Q'
+  objName: 'Q',
 };
 
 const gainvalObj = {
@@ -166,51 +176,49 @@ const gainvalObj = {
     id: 'gain',
     min: -50,
     max: 50,
-    value: 0
+    value: 0,
   },
   pObj: {
     id: 'gainval',
-    label: ''
+    label: '',
   },
-  objName: 'Gain'
+  objName: 'Gain',
 };
 
-const buttonDiv = document.createElement('div');
-      buttonDiv.style.width = '100%';
-const playnoiseButton = createButton('playnoise', 'Play Noise');
-const stopButton = createButton('stop');
-
-
-const controllerObjs = createControllerObjs([selectTypeObj, freqvalObj, qvalObj, gainvalObj]);
+const controllerObjs = createControllerObjs([
+  selectTypeObj,
+  freqvalObj,
+  qvalObj,
+  gainvalObj,
+]);
 const controllerTable = createControllerTable(controllerObjs);
 
 /* appendChild document element */
-const nodeArray = [mainTitleHeader, buttonDiv, [playnoiseButton, stopButton], controllerTable];
-
-setAppendChild(nodeArray);
-
-
-
-
-
+setAppendChild([
+  mainTitleHeader,
+  buttonDiv,
+  [playnoiseButton, stopButton],
+  controllerTable,
+]);
 
 // todo: MouseEvent TouchEvent wrapper
 const { touchBegan, touchMoved, touchEnded } = {
-  touchBegan: typeof document.ontouchstart !== 'undefined' ? 'touchstart' : 'mousedown',
-  touchMoved: typeof document.ontouchmove !== 'undefined' ? 'touchmove' : 'mousemove',
-  touchEnded: typeof document.ontouchend !== 'undefined' ? 'touchend' : 'mouseup',
+  touchBegan:
+    typeof document.ontouchstart !== 'undefined' ? 'touchstart' : 'mousedown',
+  touchMoved:
+    typeof document.ontouchmove !== 'undefined' ? 'touchmove' : 'mousemove',
+  touchEnded:
+    typeof document.ontouchend !== 'undefined' ? 'touchend' : 'mouseup',
 };
-
 
 /* audio */
 const bufsize = 1024;
 const audioctx = new AudioContext();
 const scrproc = audioctx.createScriptProcessor(bufsize);
-      scrproc.onaudioprocess = Process;
-      scrproc.connect(audioctx.destination);
+scrproc.onaudioprocess = Process;
+scrproc.connect(audioctx.destination);
 let osc = null;
 let play = 0;
-
 
 function Process(ev) {
   const buf0 = ev.outputBuffer.getChannelData(0);
@@ -220,14 +228,8 @@ function Process(ev) {
   }
 }
 
-
-
-
-
-
-
 playnoiseButton.addEventListener(touchBegan, () => {
-  if(osc === null) {
+  if (osc === null) {
     osc = new OscillatorNode(audioctx);
     osc.start();
   }
@@ -237,9 +239,3 @@ playnoiseButton.addEventListener(touchBegan, () => {
 stopButton.addEventListener(touchBegan, () => {
   play = 0;
 });
-
-
-
-
-
-
