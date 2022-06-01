372333a73ef53aa6c635e3579cc01646a76c6d52
pome-ta <53405097+pome-ta@users.noreply.github.com>
2022-06-01 09:00:15 +0900

    call noise

--- docs/js/app.js
+++ docs/js/app.js
@@ -6,6 +6,12 @@ function capitalize(str) {
   return str.charAt(0).toUpperCase() + str.slice(1).toLowerCase();
 }
 
+function parseNum(value, numtype = 'float') {
+  return numtype === 'int'
+    ? Number.parseInt(value)
+    : Number.parseFloat(value).toFixed(1);
+}
+
 /* create document node element funcs */
 function createButton(idName, textContent = null) {
   const element = document.createElement('button');
@@ -23,19 +29,20 @@ function createLabel(pObj, textContent = null) {
   element.id = id;
   element.style.margin = '0';
   element.style.minWidth = '3rem';
-  element.textContent = textContent ? textContent : capitalize(id);
+  element.textContent = textContent != null ? textContent : capitalize(id);
   return element;
 }
 
 function createInputRange(rangeObj) {
-  const { id, min, max, value, step = '' } = rangeObj;
+  const { id, min, max, value, numtype, step = '' } = rangeObj;
   const element = document.createElement('input');
   element.type = 'range';
   element.id = id;
   element.min = min;
   element.max = max;
-  element.step = step;
   element.value = value;
+  element.numtype = numtype;
+  element.step = step;
   element.style.width = '100%';
   return element;
 }
@@ -69,7 +76,9 @@ function createControllerObjs(objArray) {
       ? createInputRange(obj[inputObj])
       : null;
 
-    const _label = inputElement ? inputElement.value : null;
+    const _label = inputElement
+      ? parseNum(inputElement.value, inputElement.numtype)
+      : null;
     const pElement = Object.keys(obj).some((key) => key === pObj)
       ? createLabel(obj[pObj], _label)
       : null;
@@ -160,6 +169,7 @@ const freqvalObj = {
     min: 100,
     max: 20000,
     value: 5000,
+    numtype: 'int',
   },
   pObj: {
     id: 'freqval',
@@ -175,6 +185,7 @@ const qvalObj = {
     max: 50.0,
     step: 0.5,
     value: 5.0,
+    numtype: 'float',
   },
   pObj: {
     id: 'qval',
@@ -189,6 +200,7 @@ const gainvalObj = {
     min: -50,
     max: 50,
     value: 0,
+    numtype: 'int',
   },
   pObj: {
     id: 'gainval',
@@ -228,41 +240,48 @@ const { touchBegan, touchMoved, touchEnded } = {
 };
 
 /* audio */
-const bufsize = 1024;
 const audioctx = new AudioContext();
-const scrproc = audioctx.createScriptProcessor(bufsize);
-scrproc.onaudioprocess = Process;
-scrproc.connect(audioctx.destination);
-let osc = null;
-let play = 0;
+let src = null;
+const noisebuff = new AudioBuffer({
+  channels: 1,
+  length: audioctx.sampleRate,
+  sampleRate: audioctx.sampleRate,
+});
+const filter = new BiquadFilterNode(audioctx,{frequency:5000, q:5});
+const analyser = new AnalyserNode(audioctx, {
+  smoothingTimeConstant: 0.7,
+  fftSize: 1024,
+});
+filter.connect(analyser).connect(audioctx.destination);
 
-function Process(ev) {
-  const buf0 = ev.outputBuffer.getChannelData(0);
-  const buf1 = ev.outputBuffer.getChannelData(1);
-  for (let i = 0; i < bufsize; ++i) {
-    buf0[i] = buf1[i] = (Math.random() - 0.5) * play;
+const noisebuffdata = noisebuff.getChannelData(0);
+for(let i = 0; i < audioctx.sampleRate; i++) {
+  noisebuffdata[i] = (Math.random() - 0.5) * 0.5;
+}
+
+
+playnoiseButton.addEventListener(touchBegan, () => {
+  (audioctx.state === "suspended") ? audioctx.resume():null;
+  (src) ? src.stop():null;
+  src = new AudioBufferSourceNode(audioctx, {buffer: noisebuff, loop:true});
+  src.connect(filter);
+  src.start();
+});
+
+stopButton.addEventListener(touchBegan, () => {
+  if (src) {
+    src.stop();
+    src = null;
   }
-}
-
+});
+        
 function Setup() {
-  freqval.textContent = freq.value;
-  qval.textContent = q.value;
-  gainval.textContent = gain.value;
+  freqval.textContent = parseNum(freq.value, freq.numtype);
+  qval.textContent = parseNum(q.value, q.numtype);
+  gainval.textContent = parseNum(gain.value, gain.numtype);
 }
-
 
 freq.addEventListener('input', Setup);
 q.addEventListener('input', Setup);
 gain.addEventListener('input', Setup);
 
-playnoiseButton.addEventListener(touchBegan, () => {
-  if (osc === null) {
-    osc = new OscillatorNode(audioctx);
-    osc.start();
-  }
-  play = 1;
-});
-
-stopButton.addEventListener(touchBegan, () => {
-  play = 0;
-});
